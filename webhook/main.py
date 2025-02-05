0bbe295e397c766e7e2683b25aaa8e202b8dcce7
Tayomide <tibrahim@hawk.iit.edu>
2025-02-05 17:20:10 -0600

    Update webhook logic

--- webhook/main.py
+++ webhook/main.py
@@ -1,5 +1,11 @@
-import requests
+import requests, os
 from flask import Flask, request
+from dotenv import load_dotenv
+
+load_dotenv()
+
+discord_webhook_url = os.getenv("DISCORD_WEBHOOK_URL")
+
 
 app = Flask(__name__)
 
@@ -10,21 +16,35 @@ def index():
 
 @app.route("/webhook/github", methods=["POST"])
 def github_webhook():
-  data = request.json
-  print(data)
-  # commit_messages = "\n".join(
-  #   [commit["message"] for commit in data["commits"]]
-  # )
-  # basecamp_payload = {
-  #     "content": f"New GitHub commits:\n{commit_messages}",
-  # }
-  # headers = {"Authorization": "Bearer YOUR_BASECAMP_ACCESS_TOKEN"}
-  # response = requests.post(
-  #     "https://3.basecampapi.com/YOUR_ACCOUNT_ID/messages",
-  #     json=basecamp_payload,
-  #     headers=headers,
-  # )
-  return "Testing webhook", 200
+  event_type = request.headers.get('X-GitHub-Event')
+  payload = request.json  # The JSON payload containing the event data
+
+  if event_type == 'push':
+      handle_push_event(payload)
+  elif event_type == 'pull_request':
+    handle_pull_request_event(payload)
+  else:
+    print(f"Unhandled event: {event_type}")
+
+  return "OK", 200
+
+def send_discord_message(message):
+  payload = {
+    "content": message,
+  }
+  requests.post(discord_webhook_url, json=payload)
+
+def handle_push_event(payload):
+  commit_messages = "\n".join(
+    [f"[{commit["message"]}]({commit['url']})" for commit in payload["commits"]]
+  )
+  message = f"New GitHub commits:\n{commit_messages}\n" + f"from [{payload['pusher']['name']}](https://github.com/{payload['pusher']['name']})"
+  send_discord_message(message)
+
+def handle_pull_request_event(payload):
+  message = f"New GitHub pull request: [{payload['pull_request']['title']}](https://github.com/{payload['repository']['full_name']}/pull/{payload['pull_request']['number']})\n" + f"from [{payload['pull_request']['user']['login']}](https://github.com/{payload['pull_request']['user']['login']})"
+
+  send_discord_message(message)
 
 if __name__ == "__main__":
   app.run(port=5000)
