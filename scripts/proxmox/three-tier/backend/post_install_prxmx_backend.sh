575dc749f11153c7e1a8b5ec09ebf37030fe7af3
Tayomide <tibrahim@hawk.iit.edu>
2025-03-02 21:13:19 -0600

    Update script for resilience

--- scripts/proxmox/three-tier/backend/post_install_prxmx_backend.sh
+++ scripts/proxmox/three-tier/backend/post_install_prxmx_backend.sh
@@ -6,19 +6,19 @@ sudo apt-get update
 sudo apt-get install -y curl rsync
 
 # Install python
-sudo apt install python3
+sudo apt install -y python3 python3-venv python3-pip
 
 # Change directory to the location of your JS code
 cd /home/vagrant/spring2025-team05/server
 
 # Setup virtual environment
 python3 -m venv venv
-source venv/bin/activate
+. venv/bin/activate
 pip install -r requirements.txt
 
 # TODO: Add environment variables to .env file
 # cp .template-env .env
 # sed...
 
-# Start the server
-uvicorn app:app --host 0.0.0.0 --port 8000
+# Start the server with nohup for persistence
+nohup uvicorn app:app --host 0.0.0.0 --port 8000 > server.log 2>&1 &
