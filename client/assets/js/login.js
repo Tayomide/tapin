aa5c42a46fc6b7e383189eb76696ab432f41cc96
Tayomide <tibrahim@hawk.iit.edu>
2025-03-11 15:35:09 -0500

    Update login syntax

--- client/assets/js/login.js
+++ client/assets/js/login.js
@@ -1,27 +1,26 @@
 document.getElementById("studentSubmit").onclick = function validateForm() {
-    const containsA = /A/g;
-    const containsEightNum = /\d{8}/g;
+  const containsA = /A/g;
+  const containsEightNum = /\d{8}/g;
 
+  const sName = document.forms["studentForm"]["studentName"].value;
+  const sId = document.forms["studentForm"]["studentId"].value;
+  if (sName == "") {
+    const errorField = document.getElementById("error");
+    errorField.textContent = "Student Name is empty!";
+    return false;
+  } else if (sId == "") {
+    const errorField = document.getElementById("error");
+    errorField.textContent = "Student ID is empty!";
+    return false;
+  } else if (containsA.test(sId) == false) {
+    const errorField = document.getElementById("error");
+    errorField.textContent = "Student ID must contain letter 'A'!";
+    return false;
+  } else if (containsEightNum.test(sId) == false) {
+    const errorField = document.getElementById("error");
+    errorField.textContent = "Student ID must contain eight numbers!";
+    return false;
+  }
 
-    let sName = document.forms["studentForm"]["studentName"].value;
-    let sId = document.forms["studentForm"]["studentId"].value;
-    if (sName == "") {
-        var errorField = document.getElementById("error");
-        errorField.textContent = "Student Name is empty!";
-        return false;
-    } else if (sId == "") {
-        var errorField = document.getElementById("error");
-        errorField.textContent = "Student ID is empty!";
-        return false;
-    } else if (containsA.test(sId) == false) {
-        var errorField = document.getElementById("error");
-        errorField.textContent = "Student ID must contain letter 'A'!";
-        return false;
-    } else if (containsEightNum.test(sId) == false) {
-        var errorField = document.getElementById("error");
-        errorField.textContent = "Student ID must contain eight numbers!";
-        return false;
-    }
-
-    window.location.href = "/training_complete";
+  window.location.href = "/training_complete";
 };
