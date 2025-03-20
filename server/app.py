7e44dd3758c09416ce05dd883cac1a30e87dfe32
Tayomide <tibrahim@hawk.iit.edu>
2025-03-20 02:30:55 -0500

    Update server logic

--- server/app.py
+++ server/app.py
@@ -1,15 +1,21 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI, Response, Request
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
+load_dotenv()
+
 app = FastAPI()
 
+PORT = int(os.getenv("PORT", 8000))
+
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["https://70d9-2620-f3-8000-5050-42-a658-b52f-b35b.ngrok-free.app"],  # You can restrict this to your ngrok URL or client domain
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+  CORSMiddleware,
+  allow_origins=["https://70d9-2620-f3-8000-5050-42-a658-b52f-b35b.ngrok-free.app"],  # You can restrict this to your ngrok URL or client domain
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
 )
 
 @app.get("/")
@@ -35,3 +41,18 @@ async def get_cookie(request: Request):
   cookie_value = request.cookies.get("my_cookie", "Cookie not found")
   print(cookie_value, request.cookies)
   return {"cookie_value": cookie_value}
+
+
+if __name__ == "__main__":
+  import uvicorn
+  ssl_folder = "/workspace/ssl"
+  if os.path.isdir(ssl_folder):
+    uvicorn.run(
+      app,
+      host="0.0.0.0",  # Listen on all interfaces
+      port=PORT,       # Specify the port
+      ssl_certfile="/workspace/ssl/devcontainer.crt",  # Path to your certificate
+      ssl_keyfile="/workspace/ssl/devcontainer.key"    # Path to your private key
+    )
+  else:
+    uvicorn.run(app, host="0.0.0.0", port=PORT)
