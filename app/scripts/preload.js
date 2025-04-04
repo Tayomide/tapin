c39c3a0e38d608208f0c83c83fc6981feb6966a8
Tayomide <tibrahim@hawk.iit.edu>
2025-04-04 04:32:20 -0500

    Add event listeners for card reader on client

--- app/scripts/preload.js
+++ app/scripts/preload.js
@@ -1,3 +1,13 @@
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  onReaderDetected: (callback) => ipcRenderer.on('reader-detected', (_, name) => callback(name)),
+  onCardInserted: (callback) => ipcRenderer.on('card-inserted', (_, uid) => callback(uid)),
+  onCardRemoved: (callback) => ipcRenderer.on('card-removed', () => callback()),
+  onReaderRemoved: (callback) => ipcRenderer.on('reader-removed', (_, name) => callback(name)),
+  onPcscError: (callback) => ipcRenderer.on('pcsc-error', () => callback())
+});
+
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
