a09318529728fb1f78e9051c093862f333838349
Tayomide <tibrahim@hawk.iit.edu>
2025-04-07 20:15:37 -0500

    Use pm2 to manage server

--- scripts/proxmox/three-tier/backend/post_install_prxmx_backend.sh
+++ scripts/proxmox/three-tier/backend/post_install_prxmx_backend.sh
@@ -8,16 +8,26 @@ sudo apt-get install -y curl rsync
 # Install python
 sudo apt install -y python3 python3-venv python3-pip
 
+# Steps to add NodeJS repository to your Ubuntu Server for Node and NPM installation
+# Remove and or replace with your required webserver stack
+# https://github.com/nodesource/distributions/blob/master/README.md#using-ubuntu-2
+curl -fsSL https://deb.nodesource.com/setup_22.x -o nodesource_setup.sh
+sudo -E bash nodesource_setup.sh
+sudo apt-get install -y nodejs
+
+# Use NPM package manager to install needed dependecies to run our EJS app
+# https://github.com/motdotla/dotenv -- create a .env file to pass environment variables
+# dotenv mysql2 packages will be installed in the package.json file
+sudo npm install -g --save express ejs pm2
+
 # Change directory to the location of your JS code
 cd /home/vagrant/spring2025-team05/server
 
 # Setup virtual environment
 python3 -m venv venv
-. venv/bin/activate
-pip install -r requirements.txt
+# . venv/bin/activate
+./venv/bin/pip install -r requirements.txt
 
-# Start the server with nohup for persistence
-nohup python app.py > server.log 2>&1 &
 
 cp --update=none .template-env .env
 
@@ -33,3 +43,9 @@ sudo sed -i "s/OAUTH_REDIRECT_URI=/OAUTH_REDIRECT_URI=$ESCAPED_OAUTH_REDIRECT_UR
 ESCAPED_SECRET_KEY=$(printf '%s\n' "$SECRET_KEY" | sed 's/[/&|]/\&/g')
 sudo sed -i "s/SECRET_KEY=/SECRET_KEY=$ESCAPED_SECRET_KEY/" /home/vagrant/spring2025-team05/server/.env
 sudo sed -i "s/EXPIRES_IN=/EXPIRES_IN=$EXPIRES_IN/" /home/vagrant/spring2025-team05/server/.env
+
+pm2 start "./venv/bin/python3 app.py" --name server
+
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u vagrant --hp /home/vagrant
+
+sudo -u vagrant pm2 save
