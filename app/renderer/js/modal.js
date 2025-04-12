6f5a8026480ed542b41265fa17348a5cc1a4fa16
Tayomide <tibrahim@hawk.iit.edu>
2025-04-12 13:54:55 -0500

    Create modal

---
+++ app/renderer/js/modal.js
@@ -0,0 +1,69 @@
+const createModal = () => {
+  const modal = document.createElement('div')
+  modal.classList.add('modal')
+
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      deleteModal(modal)
+    }
+  })
+
+  const modalContent = document.createElement('div')
+  modalContent.classList.add('modal-content')
+
+  const h2 = document.createElement('h2')
+  h2.textContent = 'Link Card'
+
+  const p1 = document.createElement('p')
+  p1.textContent = 'Your hashed card ID doesn\'t exist in the database.'
+
+  const p2 = document.createElement('p')
+  p2.textContent = 'Would you like to link it?'
+
+  const input = document.createElement('input')
+  input.type = 'text'
+  input.id = 'card-id'
+  input.placeholder = 'Enter your hawk email here'
+
+  const modalButtons = document.createElement('div')
+  modalButtons.classList.add('modal-buttons')
+
+  const cancelLink = document.createElement('button')
+  cancelLink.id = 'cancel-link'
+  cancelLink.textContent = 'Cancel'
+  cancelLink.addEventListener('click', (e) => {
+    e.stopPropagation() // Prevent the click event from bubbling up to the modal
+    deleteModal(modal)
+  })
+
+  const linkCard = document.createElement('button')
+  linkCard.id = 'link-card'
+  linkCard.textContent = 'Link'
+  linkCard.addEventListener('click', (e) => {
+    e.stopPropagation() // Prevent the click event from bubbling up to the modal
+    deleteModal(modal)
+  })
+
+  modalButtons.appendChild(cancelLink)
+  modalButtons.appendChild(linkCard)
+
+  modalContent.appendChild(h2)
+  modalContent.appendChild(p1)
+  modalContent.appendChild(p2)
+  modalContent.appendChild(input)
+  modalContent.appendChild(modalButtons)
+
+  modal.appendChild(modalContent)
+
+  document.body.appendChild(modal)
+}
+
+const deleteModal = (modal) => {
+  if (modal) {
+    modal.remove()
+    modal = null
+  }
+}
+
+// Export the functions to be used in other files
+export { createModal, deleteModal }
