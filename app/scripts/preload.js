ce9eb935bf371b848542229643cf21e9f47cc19a
Tayomide <tibrahim@hawk.iit.edu>
2025-04-12 13:54:38 -0500

    Port custom function to renderer

--- app/scripts/preload.js
+++ app/scripts/preload.js
@@ -1,20 +1,21 @@
-const { contextBridge, ipcRenderer } = require('electron')
+const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
   onReaderDetected: (callback) => ipcRenderer.on('reader-detected', (_, name) => callback(name)),
   onCardInserted: (callback) => ipcRenderer.on('card-inserted', (_, uid) => callback(uid)),
   onCardRemoved: (callback) => ipcRenderer.on('card-removed', () => callback()),
   onReaderRemoved: (callback) => ipcRenderer.on('reader-removed', (_, name) => callback(name)),
-  onPcscError: (callback) => ipcRenderer.on('pcsc-error', () => callback())
+  onPcscError: (callback) => ipcRenderer.on('pcsc-error', () => callback()),
+  hashHex: (hex) => ipcRenderer.invoke('hash-hex', hex),
 });
 
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+// window.addEventListener('DOMContentLoaded', () => {
+//   const replaceText = (selector, text) => {
+//     const element = document.getElementById(selector)
+//     if (element) element.innerText = text
+//   }
 
-  for (const dependency of ['chrome', 'node', 'electron']) {
-    replaceText(`${dependency}-version`, process.versions[dependency])
-  }
-})
+//   for (const dependency of ['chrome', 'node', 'electron']) {
+//     replaceText(`${dependency}-version`, process.versions[dependency])
+//   }
+// })
