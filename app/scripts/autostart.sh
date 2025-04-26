54a707cace2850c98eb6084c9e9330e331d114a5
PolarBear78 <jjarjourah@gmail.com>
2025-04-26 18:01:41 -0500

    Update autostart.sh

--- app/scripts/autostart.sh
+++ app/scripts/autostart.sh
@@ -1,20 +1,21 @@
-# /usr/local/bin/start‑spring‑app.sh
+# /usr/local/bin/startâ€‘springâ€‘app.sh
 #!/usr/bin/env bash
 set -e
 
 # 1) Make sure the cluster service you need is up
-sudo systemctl start pcsd.service        # …and enable it once so it survives reboots
+sudo systemctl start pcscd.service        # â€¦and enable it once so it survives reboots
                                           #   sudo systemctl enable pcsd.service
 
 # 2) run the Electron/Node app
-cd /project/spring/app
+cd /home/pi/project/spring2025-team05/app
 npm run dev &                             # launches Electron
 
 APP_PID=$!
 
-# 3)  optional: shove the window to full‑screen
-#     (F11 is the universal Electron “toggle full‑screen”)
+# 3)  optional: shove the window to fullâ€‘screen
+#     (F11 is the universal Electron â€œtoggle fullâ€‘screenâ€)
 sleep 8                                   # give the window time to appear
 xdotool search --onlyvisible --name "Electron" windowactivate --sync key F11
 
 wait "$APP_PID"
+
